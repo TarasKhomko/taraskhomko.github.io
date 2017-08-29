a67c6a90aa01499fb32b54f074394aa9747f525c
TarasKhomko <taras.khomko@gmail.com>
2017-08-29 14:05:41 +0300

    create cv

--- js/js.js
+++ js/js.js
@@ -1,6 +1,9 @@
-
+$(document).ready(function(){
 	$('.skil').each(function(){
 		$(this).find('.skil_bar').animate({
 			width:$(this).attr('data-percent')
 		},6000);
 	});
+
+ $(" a").mPageScroll2id({offset: 52,scrollSpeed: 15});
+		});
